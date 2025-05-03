5359f6f08037878e34bb60fb3e20c35f577924d0
AlexLosciale <losciale17@gmail.com>
2025-05-03 12:33:33 +0200

    add: snack5

---
+++ script.js
@@ -0,0 +1,60 @@
+/**
+ * Snack 1
+ * Crea una funzione che somma due numeri.
+ */
+function somma(a, b) {
+    return a + b;
+}
+
+/**
+ * Snack 2
+ * Crea una arrow function che calcola il quadrato di un numero.
+ * Definisci una funzione chiamata `quadrato` che accetta un numero e restituisce il suo quadrato in una sola riga.
+ */
+const quadrato = (numero) => numero * numero;
+
+/**
+ * Snack 3
+ * Crea una funzione `eseguiOperazione`.
+ * Definisci una funzione che accetta tre parametri: due numeri e una funzione operatore (callback).
+ * La funzione deve eseguire l'operazione fornita sui due numeri.
+*/
+function eseguiOperazione(num1, num2, operazione) {
+    return operazione(num1, num2);
+}
+
+/**
+ * Snack 4
+ * Crea un generatore di funzioni `creaTimer`.
+ * Scrivi una funzione che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+*/
+function creaTimer(tempo) {
+    return function() {
+        setTimeout(() => {
+            console.log("Tempo scaduto!");
+        }, tempo);
+    };
+}
+/**
+ * Snack 5
+ * Crea una funzione `stampaOgniSecondo` con setInterval.
+ * Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+*/
+function stampaOgniSecondo(messaggio) {
+    setInterval(() => {
+        console.log(messaggio);
+    }, 1000);
+}
+
+/**
+ * Snack 6
+ * Crea un contatore automatico con setInterval.
+ * Definisci una funzione `creaContatoreAutomatico` che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval,
+ * incrementando un contatore e stampandolo.
+
+
+/**
+ * Snack 7
+ * Crea una funzione che ferma un timer dopo un certo tempo.
+ * Scrivi una funzione `eseguiEferma` che accetta un messaggio, un tempo di avvio e un tempo di stop.
+ * Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
